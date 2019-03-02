c3b55ba8d312b66dc9fe581b41e084860a01217f
pe-to <taku.star.ring.child@gmail.com>
2019-03-02 18:08:36 +0900

    問題1-3を解きました。

---
+++ 01/Kantan.java
@@ -0,0 +1,8 @@
+public class Kantan {
+    public static void main(String[] args) {
+        System.out.println("意外に\"java\"って簡単だ。");
+        System.out.println("意外に\"java\"って簡単だ。");
+        System.out.println("意外に\"java\"って簡単だ。");
+
+    }
+}
